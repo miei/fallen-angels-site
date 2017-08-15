9218bf39f237bef7c268e9f883e12ddb2d25eabc
miei <mieolson@outlook.com>
2017-08-15 11:22:05 +0100

    clean up subscription

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { NavLinksService } from '../shared/nav-links.service';
 import { AuthService } from '../auth/auth.service';
 
@@ -7,19 +7,25 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   links = [];
   userInfo;
+  userInfoSubscription;
 
   constructor(private navLinksService: NavLinksService, public authService: AuthService) {
     this.links = navLinksService.links;
   }
 
   ngOnInit() {
-    this.authService.user.subscribe(user => {
+    this.userInfoSubscription = this.authService.user.subscribe(user => {
       console.log(user);
       this.userInfo = user;
     });
   }
 
+  ngOnDestroy() {
+    if (this.userInfoSubscription) {
+      this.userInfoSubscription.unsubscribe();
+    }
+  }
 }
